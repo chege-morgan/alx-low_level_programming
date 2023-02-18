7d6fa18a9af3d36a13a1bb6f21595024f34aa0a4
chege-morgan <chegemorgan05@gmail.com>
2023-02-18 01:43:58 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float f;
 
 	Printf("size of a int: %zu byte(s)\n", sizeof(int));
-	Printf("size of a long int: %zu byte(s)\n", sizeof(blong int));
+	Printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	Printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	Printf("size of a char: %zu byte(s)\n", sizeof(char));
 	Printf("size of a float: %zu byte(s)\n", sizeof(float));

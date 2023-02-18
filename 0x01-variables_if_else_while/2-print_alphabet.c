f8ff74a7e33c34a1ddef4c0302ffd25860d50a08
chege-morgan <chegemorgan05@gmail.com>
2023-02-18 07:41:01 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+
+{
+int main(void)
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'Z') {
+			putchar(c);
+			c++
+		}
+	while
+		(d <= 'z') {
+			putchar(c);
+			c++
+		}
+	Return(0);
+}

2467b0488f16950f9b05a473b3f895878ddfb1ea
chege-morgan <chegemorgan05@gmail.com>
2023-02-23 00:53:57 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = '0' ; m <= '9' ; m++)
+	putchar(m);
+	putchar('\n');
+
+	return (0);
+}

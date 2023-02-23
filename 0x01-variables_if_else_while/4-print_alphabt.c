b4577e98c34611a9e52258d563ac83ce8c92ba22
chege-morgan <chegemorgan05@gmail.com>
2023-02-22 23:40:10 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main Function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	x <= 'z';
+	for (x = 'a' ; 'z'; x++)
+	if (x != 'q' && x != 'e')
+		putchar(x);
+		putchar('\n');
+		return (0);
+}

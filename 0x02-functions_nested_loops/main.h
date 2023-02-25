7faa412cc88f9cf3c1a975a283cb4d49577d3311
chege-morgan <chegemorgan05@gmail.com>
2023-02-25 01:21:42 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef main_h
 #define main_h
--void print_alphabet(void);
+
+int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);

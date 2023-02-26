04f7a1f1ebed49c7400f1f3000b8d989e7d6619a
chege-morgan <chegemorgan05@gmail.com>
2023-02-26 05:31:37 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add 2 intengers
+ * @num1: first integer
+ * @num2:  second integer
+ * Return: dum of int 1& 2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
